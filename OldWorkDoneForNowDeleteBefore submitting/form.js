94236dd69755335793a34a4a376d3c50732c4464
boosbaas <jbegonya@protonmail.com>
2020-03-01 13:01:28 -0500

    necessary clean up

--- OldWorkDoneForNowDeleteBefore submitting/form.js
+++
@@ -1,3 +0,0 @@
-// let contactUs = document.getElementById("contactUsForm");
-// let createform = document.createElement("contactUsForm"); // Create New Element Form
-// contactUs.appendChild(createform);
